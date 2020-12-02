3374696d8f9b938f27ecd871af76169fdaacd8d5
m19e <nexus.me7290@gmail.com>
2020-12-03 00:24:13 +0900

    WIP

--- main.go
+++ main.go
@@ -19,17 +19,37 @@ func init() {
 
 func main() {
 	target := os.Getenv("TARGET_URL")
+	// doc, err := goquery.NewDocument(target)
 
-	doc, err := goquery.NewDocument(target)
+	reader := readLocalFile("root.html")
+	doc, err := goquery.NewDocumentFromReader(reader)
+
 	if err != nil {
 		fmt.Print("url scraping failed")
 	}
+
+	branchs := []string{}
+
 	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
 		url, _ := s.Attr("href")
 		if !strings.Contains(url, "/") {
-			fmt.Println(url)
+			branchs = append(branchs, url)
 		}
 	})
+
+	for _, b := range branchs[:1] {
+		fmt.Println(fmt.Sprintf("%s%s", target, b))
+		d, err := goquery.NewDocument(fmt.Sprintf("%s%s", target, b))
+		if err != nil {
+			log.Fatal(err)
+		}
+		h, err := d.Html()
+		fmt.Println(h)
+		d.Find("a").Each(func(_ int, s *goquery.Selection) {
+			url, _ := s.Attr("href")
+			fmt.Println(url)
+		})
+	}
 }
 
 func writeLocalFile(filename, data string) error {
